e3a122231790532ec16e1083a2ffafb0f102c1a4
unknown <raffy.amoguis.projects@gmail.com>
2023-05-22 22:17:02 +0800

    updated header contents

--- src/components/Headcontent.tsx
+++ src/components/Headcontent.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Group, Text } from '@mantine/core';
-// import { useLocation } from 'react-router-dom';
+import { Group, Text, ActionIcon, TextInput } from '@mantine/core';
+import { IconBrightness, IconSearch } from '@tabler/icons-react';
 import useRouteLocation from '../hooks/useRouteLocation';
 
 const Headcontent: React.FC = () => {
   const routeName = useRouteLocation();
   return (
-    <Group position='apart'>
+    <Group position='apart' sx={{ width: '100%' }}>
       <Text>{routeName}</Text>
+      {routeName === 'Home' ? (
+        <TextInput
+          sx={{ width: '400px' }}
+          placeholder='Look for anime'
+          icon={<IconSearch size='0.8rem' />}
+          variant='filled'
+        />
+      ) : null}
+      <ActionIcon variant='transparent'>
+        <IconBrightness />
+      </ActionIcon>
     </Group>
   );
 };
